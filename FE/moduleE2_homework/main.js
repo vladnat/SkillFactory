da83f247b35cd35548e4adcbc0ffd02dad66a68a
vladnat <vlad27nat10@yandex.ru>
2022-01-16 20:37:24 +0300

    added task_2

--- FE/moduleE2_homework/main.js
+++ FE/moduleE2_homework/main.js
@@ -1,4 +1,5 @@
 import { NumIsNumber } from "/task_1.js"
+import { TypeX } from "/task_2.js"
 
 
 // console.log(NumIsNumber)
@@ -9,4 +10,9 @@ import { NumIsNumber } from "/task_1.js"
 let task_1 = document.querySelector('.task_1')
 task_1.addEventListener('click', function() {
     console.log(NumIsNumber())
+})
+
+let task_2 = document.querySelector('.task_2')
+task_2.addEventListener('click', function() {
+    console.log(TypeX())
 })
