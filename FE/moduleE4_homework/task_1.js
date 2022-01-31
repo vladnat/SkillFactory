445612c13117fe66337e45df27578cb9272586d7
vladnat <vlad27nat10@yandex.ru>
2022-01-31 13:54:53 +0300

    E4 added task_1.js

---
+++ FE/moduleE4_homework/task_1.js
@@ -0,0 +1,13 @@
+// Написать, функцию, которая принимает в качестве аргумента объект
+// и выводит в консоль все ключи и значения только собственных свойств.
+// Данная функция не должна возвращать значение.
+
+function ObjectOwnPropertiesAndKeys (obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            console.log(`key - ${key}, value - ${obj[key]}`)
+        }
+    }
+}
+
+const obj = {'key_': 98}
